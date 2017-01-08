bcf1dae460306976d09c8be3e462b2fd11431094
jake-waratahs <jakebloom744@gmail.com>
2017-01-08 11:31:38 +1100

    Added bower and semantic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import 'semantic-ui/semantic.min.css'
+
 import Hello from './components/hello'
 
 render(
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HappyPack = require('happypack')
 var path = require('path')
 
+var bower_dir = path.join(__dirname, 'bower_components')
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -20,12 +22,32 @@ module.exports = {
                 },
                 recursive: true, 
                 include: [path.join(__dirname, 'src')]
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /.(png|jpg|jpeg)$/,
+                loader: 'url-loader?limit=4098'
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=\.]+)?$/,
+                loader: 'file-loader'
             }
         ]
     },
     plugins: [
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+        ),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         })
-    ]
+    ],
+    resolve: {
+        alias: {
+            'semantic-ui': bower_dir + '/semantic/dist'
+        }
+    }
 }
